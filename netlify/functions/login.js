2dbe885e4454601c95166c96b7d6587118424a56
Timur Barmakov <tbarmakov@bk.ru>
2023-10-28 21:54:58 +0600

    10th commit

---
+++ netlify/functions/login.js
@@ -0,0 +1,47 @@
+exports.handler = async (event, context) => {
+  const requestBody = JSON.parse(event.body);
+  const { password, username, type } = requestBody;
+
+  // Опционально, вы можете добавить логику ожидания
+  // await waitTime(2000);
+
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      status: 'error',
+      type,
+      currentAuthority: 'guest',
+    }),
+  };
+
+  if (password === 'a' && username === 'a') {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        status: 'ok',
+        type,
+        currentAuthority: 'admin',
+      });
+      // Если вы хотите сохранить значение access, вы можете сделать это здесь
+    } else if (password === 'ant.design' && username === 'user') {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          status: 'ok',
+          type,
+          currentAuthority: 'user',
+        });
+      // Аналогично, сохраните значение access
+    } else if (type === 'mobile') {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          status: 'ok',
+          type,
+          currentAuthority: 'admin',
+        });
+      // Сохраните access
+    }
+
+  return response;
+};
